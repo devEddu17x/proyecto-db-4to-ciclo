55aeca09c5e732935e3d1326343859a4c241695f
Eddu <azabacheeduardo85@gmail.com>
2024-11-27 09:13:23 -0500

    modified tables structure

--- eliminacion-tablas.sql
+++ eliminacion-tablas.sql
@@ -1,14 +1,17 @@
+<<<<<<< HEAD
+=======
+DROP TABLE BOLETA;
+DROP TABLE FACTURA;
+>>>>>>> ef92ee7 (modified tables structure)
 DROP TABLE Comprobante;
-DROP TABLE Detalle_Platillo;
+DROP TABLE Platillo_Pedido;
 DROP TABLE Bebida_Pedido;
-DROP TABLE Extra_Pedido;
+DROP TABLE Guarnicion_Pedido;
 DROP TABLE Platillo; 
-DROP TABLE Extra; 
+DROP TABLE Guarnicion; 
 DROP TABLE Bebida;
-DROP TABLE Cliente_Juridico;
-DROP TABLE Cliente_Natural;
 DROP TABLE Pedido;
 DROP TABLE Mozo;
 DROP TABLE Cliente;
-
+DROP TABLE Mesa;
 --TRUNCATE TABLE CLIENTE;
