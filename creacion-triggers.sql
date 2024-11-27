794ba78ff5b688a52fd6af5658f46c8aed6d1f84
Eddu <azabacheeduardo85@gmail.com>
2024-11-27 12:08:33 -0500

    triggers

--- creacion-triggers.sql
+++ creacion-triggers.sql
@@ -1,26 +1,51 @@
 -- cada trigger se debe ejecutar uno por uno para que no genere errores
 
-
 CREATE OR REPLACE TRIGGER TRG_VERIFICAR_MESA_OCUPADA
 BEFORE INSERT ON PEDIDO
 FOR EACH ROW
 DECLARE
-    mesa_estado VARCHAR2(10); -- Declaración explícita del tipo de datos
+	-- variable con el mismo tipo de dato que eestado
+    mesa_estado VARCHAR2(10); 
 BEGIN
-    -- Obtener el estado de la mesa
+    -- buscando el estado de la mesa
     SELECT ESTADO INTO mesa_estado
     FROM MESA
     WHERE NUMERO_MESA = :NEW.NUMERO_MESA;
 
-    -- Verificar si la mesa está ocupada
+    -- si la mesa está ocupada
     IF mesa_estado = 'Ocupado' THEN
         RAISE_APPLICATION_ERROR(-20001, 'La mesa ya está ocupada. Por favor elige otra mesa.');
     END IF;
 END;
-
 /
 
 
+
+CREATE OR REPLACE TRIGGER TRG_ACTUALIZAR_ESTADO_MESA
+AFTER INSERT ON PEDIDO
+FOR EACH ROW
+BEGIN
+	UPDATE MESA
+    SET ESTADO = 'Ocupado'
+    WHERE NUMERO_MESA = :NEW.NUMERO_MESA;
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER TGR_CAMBIAR_A_DISPONIBLE_MESA
+AFTER INSERT ON COMPROBANTE
+FOR EACH ROW
+BEGIN
+    -- Actualizar el estado de la mesa asociada al pedido
+    UPDATE MESA
+    SET ESTADO = 'Disponible'
+    WHERE NUMERO_MESA = (
+        SELECT NUMERO_MESA
+        FROM PEDIDO
+        WHERE ID_PEDIDO = :NEW.ID_PEDIDO
+    );
+END;
+/
 
 
 CREATE OR REPLACE TRIGGER trg_comprobante_calcular_total
@@ -30,29 +55,34 @@ DECLARE
     total NUMBER := 0;
     platillo_total NUMBER := 0;
     bebida_total NUMBER := 0;
-    extra_total NUMBER := 0;
+    guarnicion_total NUMBER := 0;
 BEGIN
-	-- hallar total de los platillos
-    SELECT COALESCE(SUM(p.precio * dp.cantidad), 0)
+    -- Hallar el total de los platillos
+    SELECT COALESCE(SUM(p.precio * pp.cantidad), 0)
     INTO platillo_total
-    FROM DETALLE_PLATILLO dp 
-    JOIN PLATILLO p ON dp.id_platillo = p.id_platillo
-    WHERE dp.id_pedido = :NEW.id_pedido;
-   -- hallar total de las bebidas
+    FROM PLATILLO_PEDIDO pp 
+    JOIN PLATILLO p ON pp.id_platillo = p.id_platillo
+    WHERE pp.id_pedido = :NEW.id_pedido;
+    
+    -- Hallar el total de las bebidas
     SELECT COALESCE(SUM(b.precio * bp.cantidad), 0)
     INTO bebida_total
     FROM BEBIDA_PEDIDO bp
     JOIN BEBIDA b ON bp.id_bebida = b.id_bebida
     WHERE bp.id_pedido = :NEW.id_pedido;
-   -- hallar total de los extras
-    SELECT COALESCE(SUM(e.precio * ep.cantidad), 0)
-    INTO extra_total
-    FROM EXTRA_PEDIDO ep 
-    JOIN EXTRA e ON ep.id_extra = e.id_extra
-    WHERE ep.id_pedido = :NEW.id_pedido;
-   -- obtener el total sumando lo anterior
-    total := platillo_total + bebida_total + extra_total;
+    
+    -- Hallar el total de las guarniciones
+    SELECT COALESCE(SUM(g.precio * gp.cantidad), 0)
+    INTO guarnicion_total
+    FROM GUARNICION_PEDIDO gp 
+    JOIN GUARNICION g ON gp.id_guarnicion = g.id_guarnicion
+    WHERE gp.id_pedido = :NEW.id_pedido;
+    
+    total := platillo_total + bebida_total + guarnicion_total;
+    
     :NEW.total := total;
 END;
 /
+
+
 
